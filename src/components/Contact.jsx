971b9a56de39205a3d3fe72442f450b4da4e2540
Vaibhav1505vkv <vaibhav@vkvtech.com>
2026-06-03 14:25:44 +0530

    updated resume url

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,7 +41,7 @@ export default function Contact() {
               className="border-zinc-300 dark:border-white/20 text-zinc-900 dark:text-white hover:bg-zinc-100 dark:hover:bg-white/10 w-full sm:w-auto px-8 font-bold"
               endContent={<ArrowUpRight size={18} />}
               as="a"
-              href="https://drive.google.com/file/d/1IW-RUspydQ9_Ydx7W3lgq6EtPzPX3Asa/view"
+              href="https://drive.google.com/file/d/1fb1lEiPeJsE91yEC5RVt34kKVqz7D-JM/view?usp=sharing"
               target="_blank"
             >
               Resume
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,7 +67,7 @@ export default function Hero() {
             className="border-zinc-300 dark:border-zinc-700 text-zinc-800 dark:text-white hover:bg-zinc-100 dark:hover:bg-zinc-800 hover:border-zinc-500 transition-all px-8 font-bold"
             startContent={<FileText size={18} />}
             as="a"
-            href="https://drive.google.com/file/d/1IW-RUspydQ9_Ydx7W3lgq6EtPzPX3Asa/view"
+            href="https://drive.google.com/file/d/1fb1lEiPeJsE91yEC5RVt34kKVqz7D-JM/view?usp=sharing"
             target="_blank"
           >
             Download Resume
